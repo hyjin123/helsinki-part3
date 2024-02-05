d138877a50b1bd92037cb92d06f2918d548a4467
Sean <seanhoyeonjin@gmail.com>
2024-02-04 22:42:28 -0500

    finished exercise 3.4

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -45,6 +47,15 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  persons = persons.filter((person) => person.id !== id);
+
+  console.log(persons);
+  response.status(204).end();
 });
 
 const PORT = 3001;
