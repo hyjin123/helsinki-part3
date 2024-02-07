53d81ea6db261d034567a78e18401fe85a69fc68
Sean <seanhoyeonjin@gmail.com>
2024-02-07 12:44:53 -0500

    finished exercise 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,6 +10,8 @@ morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
 
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(
   morgan(`:method :url :status :res[content-length] - :response-time ms :body`)
