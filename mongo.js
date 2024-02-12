315a303e793613a69f42742df3ef06bfca02e88a
Sean <seanhoyeonjin@gmail.com>
2024-02-11 20:21:55 -0500

    finished exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const personName = process.argv[3];
+const personNumber = process.argv[4];
+
+const url = `mongodb+srv://seanhoyeonjin:${password}@cluster0.rj4f5jp.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+      mongoose.connection.close();
+    });
+  });
+} else {
+  const person = new Person({ name: personName, number: personNumber });
+  person.save().then((result) => {
+    console.log(
+      `Added ${result.name}, Number: ${result.number} to the Phonebook`
+    );
+    mongoose.connection.close();
+  });
+}
