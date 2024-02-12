ed1716e4cda6ef46ba0371e488ec4636b3322c19
Sean <seanhoyeonjin@gmail.com>
2024-02-12 12:25:18 -0500

    finished exercise 3.14

--- index.js
+++ index.js
@@ -48,24 +48,33 @@ app.get("/info", (request, response) => {
   `);
 });
 
+// Fetching everyone from the phonebook
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((people) => {
     response.json(people);
   });
 });
 
+// Fetching individual person's information
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+  const id = request.params.id;
 
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
+  Person.findById(id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
+
+  // const id = Number(request.params.id);
+
+  // const person = persons.find((person) => person.id === id);
+
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
 });
 
+// Deleting a person from the phonebook
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
 
@@ -74,26 +83,39 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+// Adding a person to the phonebook
 app.post("/api/persons", (request, response) => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  const body = request.body;
 
-  const person = request.body;
-
-  person.id = maxId + 1;
-
-  // if the name or number is missing, send an error message
-  if (person.name && person.number) {
-    const duplicate = persons.find((n) => n.name === person.name);
-
-    if (duplicate) {
-      response.status(404).send("name already exists in the phonebook");
-    } else {
-      persons = persons.concat(person);
-      response.json(person);
-    }
-  } else {
-    response.status(404).send("you did not specify a name or a number");
+  if (body.name === undefined) {
+    return response.status(400).json({ error: "name is missing" });
   }
+
+  const person = new Person({ name: body.name, number: body.number });
+
+  person.save().then((result) => {
+    response.json(result);
+  });
+
+  // const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+
+  // const person = request.body;
+
+  // person.id = maxId + 1;
+
+  // // if the name or number is missing, send an error message
+  // if (person.name && person.number) {
+  //   const duplicate = persons.find((n) => n.name === person.name);
+
+  //   if (duplicate) {
+  //     response.status(404).send("name already exists in the phonebook");
+  //   } else {
+  //     persons = persons.concat(person);
+  //     response.json(person);
+  //   }
+  // } else {
+  //   response.status(404).send("you did not specify a name or a number");
+  // }
 });
 
 const PORT = process.env.PORT || 3000;
