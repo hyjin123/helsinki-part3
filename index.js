721c55600a8342ad933eac2a89bd22e8e1f06548
Sean <seanhoyeonjin@gmail.com>
2024-02-24 12:34:14 -0500

    trying to fix fly.io proxy bug but not working

--- index.js
+++ index.js
@@ -188,8 +188,7 @@ const errorHandler = (error, request, response, next) => {
 
 // this has to be the last loaded middleware.
 app.use(errorHandler);
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
